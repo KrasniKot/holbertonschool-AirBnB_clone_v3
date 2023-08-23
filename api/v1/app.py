371801af5f91e9e401600ee96b7fa4d51be38066
KrasniKot <emanueltrias9@gmail.com>
2023-08-23 11:08:51 -0700

    task 5: stat route added

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+
 @app.teardown_appcontext
 def close(E):
     """Calls storage.close() when server stops"""
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,31 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 
 @app_views.route("/status")
 def status():
     """Returns a JSON string"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    """Returns the number of objects of each type"""
+    sta = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+          }
+    return jsonify(sta)
