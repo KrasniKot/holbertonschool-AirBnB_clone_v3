566a9eacf7ca88d4e2e59479e821b37297cabe46
KrasniKot <emanueltrias9@gmail.com>
2023-08-23 09:59:21 -0700

    task 4: firt line and doc up

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
+"""This module contains the routes for app_views"""
+
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from models import storage
 
 
 @app_views.route("/status")
