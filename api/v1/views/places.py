2a522563d39f9b5225f151f6e9e3eb6f53894483
KrasniKot <emanueltrias9@gmail.com>
2023-08-24 11:00:55 -0700

    checking places view functions

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -41,47 +41,47 @@ def delete_place(place_id):
         abort(404)
 
 
-@app_views.route("/cities/<city_id>/places", methods=["POST"])
-def create_place(city_id):
-    """Creates a Place object"""
-    city = storate.get(City, city_id)
-    if not city:
-        abort(404)
-
-    data = request.get_json()
-    if not data:
-        abort(400, {"error": "Not a JSON"})
-    if 'user_id' not in data:
-        abort(400, {"error": "Missing user_id"})
-    if 'name' not in data:
-        abort(400, {"error": "Missing name"})
-
-    user = storage.get(User, data['user_id'])
-    if not user:
-        abort(404)
-
-    new_place = Place(city_id=city_id, **data)
-    new_place.save()
-
-    return jsonify(new_place.to_dict()), 201
-
-
-@app_views.route("/places/<place_id>", mehtods=["PUT"])
-def update_place(place_id):
-    """Updates a Place object"""
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404)
-
-    data = request.get_json()
-    if not data:
-        abort(400, {"error": "Not a JSON"})
-
-    # Ignore keys: id, user_id, city_id, created_at, updated_at
-    ignored_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
-    for key, value in data.items():
-        if key not in ignored_keys:
-            setattr(place, key, value)
-    place.save()
-
-    return jsonify(place.to_dict()), 200
+#@app_views.route("/cities/<city_id>/places", methods=["POST"])
+#def create_place(city_id):
+#    """Creates a Place object"""
+#    city = storate.get(City, city_id)
+#    if not city:
+#        abort(404)
+#
+#    data = request.get_json()
+#    if not data:
+#        abort(400, {"error": "Not a JSON"})
+#    if 'user_id' not in data:
+#        abort(400, {"error": "Missing user_id"})
+#    if 'name' not in data:
+#        abort(400, {"error": "Missing name"})
+#
+#    user = storage.get(User, data['user_id'])
+#    if not user:
+#        abort(404)
+#
+#    new_place = Place(city_id=city_id, **data)
+#    new_place.save()
+#
+#    return jsonify(new_place.to_dict()), 201
+#
+#
+#@app_views.route("/places/<place_id>", mehtods=["PUT"])
+#def update_place(place_id):
+#    """Updates a Place object"""
+#    place = storage.get(Place, place_id)
+#    if not place:
+#        abort(404)
+#
+#    data = request.get_json()
+#    if not data:
+#        abort(400, {"error": "Not a JSON"})
+#
+#    # Ignore keys: id, user_id, city_id, created_at, updated_at
+#    ignored_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+#    for key, value in data.items():
+#        if key not in ignored_keys:
+#            setattr(place, key, value)
+#    place.save()
+#
+#    return jsonify(place.to_dict()), 200
